1173b5f1f66f5cecd8819cce7bba6d7ad777dd46
Infarh <shmachilin@yandex.ru>
2020-04-09 12:34:51 +0300

    Добавьте файлы проекта.

---
+++ TestWPFApp/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TestWPFApp
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            //MessageBox.Show("Приложение запущено");
+        }
+
+        private void Application_Exit(object Sender, ExitEventArgs E)
+        {
+            //MessageBox.Show("Приложение выгружается...");
+        }
+    }
+}
